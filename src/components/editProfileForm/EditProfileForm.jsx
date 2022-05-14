714dac947ddfe3eda541e381a2fb81b221247356
aishwarya-bm <biradaraishwarya@gmail.com>
2022-05-15 02:21:39 +0530

    added modal for edit profile

--- src/components/editProfileForm/EditProfileForm.jsx
+++ src/components/editProfileForm/EditProfileForm.jsx
@@ -1,40 +1,116 @@
-import { Box, IconButton, Modal, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  Input,
+  InputLabel,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import "./editProfileForm.css"
-export function EditProfileForm({open,handleCloseModal}){
-    return (
-      <>
-        <Modal
-          open={open}
-          onClose={handleCloseModal}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box className="modal-edit-profile">
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Edit profile
-              </Typography>
-              <IconButton
-                aria-label="close"
-                onClick={() => handleCloseModal()}
-                size="small"
-              >
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            </Box>
-            <Box sx={{display:"flex", flexDirection:"column"}}>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-              </Typography>
-            </Box>
+import "./editProfileForm.css";
+import { Label } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+export function EditProfileForm({ open, handleCloseModal }) {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+  };
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleCloseModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Edit profile
+            </Typography>
+            <Avatar
+              className="profile-cover"
+              src="https://picsum.photos/200/300"
+              sx={{ width: 56, height: 56 }}
+            ></Avatar>
           </Box>
-        </Modal>
-      </>
-    );
+        </DialogTitle>
+
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+                color="secondary"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                color="secondary"
+              />
+              <TextField
+                margin="normal"
+                fullWidth
+                id="website"
+                label="Website"
+                name="website"
+                autoComplete="website"
+                autoFocus
+                color="secondary"
+              />
+              <TextField
+                margin="normal"
+                fullWidth
+                name="bio"
+                label="Bio"
+                type="bio"
+                id="bio"
+                autoComplete="current-bio"
+                color="secondary"
+              />
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} autoFocus variant="contained">
+            Update
+          </Button>
+          <Button
+            onClick={handleCloseModal}
+            variant="outlined"
+            color="secondary"
+          >
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
